f369aa693ae67b6a0c12476d52090a3f36b1c0e0
USER <user@kohei.local>
2017-07-11 22:22:04 +0900

    “課題提出用”

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,5 +1,16 @@
 class PoemsController < ApplicationController
+  before_action :set_poem, only: [:show]
+
   def index
     @poems = Poem.all
   end
+
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_poem
+      @poem = Poem.find(params[:id])
+    end
 end
